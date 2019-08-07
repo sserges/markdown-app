d79a21b18bca392c9abe7824dd763a0685723122
sserges <agounkeserge@gmail.com>
2019-08-07 16:44:53 +0000

    Import Custom JavaScript

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
 
+import { sampleText } from "./sampleText";
+
 function App() {
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm-6">
-          <textarea className="form-control" rows="35" />
+          <textarea value={sampleText} className="form-control" rows="35" />
         </div>
         <div className="col-sm-6">
-          <h1>Résultat</h1>
+          <div>{sampleText}</div>
         </div>
       </div>
     </div>
