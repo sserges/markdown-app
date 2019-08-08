1f25b70e2b301f4c8daee1fcaa996fbfd605307f
sserges <agounkeserge@gmail.com>
2019-08-08 10:37:11 +0000

    use and update the state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 import { sampleText } from "./sampleText";
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-6">
-          <textarea value={sampleText} className="form-control" rows="35" />
-        </div>
-        <div className="col-sm-6">
-          <div>{sampleText}</div>
+class App extends Component {
+  state = {
+    text: sampleText
+  };
+
+  handleChange = event => {
+    const text = event.target.value;
+    this.setState({ text });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea
+              onChange={this.handleChange}
+              value={this.state.text}
+              className="form-control"
+              rows="35"
+            />
+          </div>
+          <div className="col-sm-6">
+            <div>{this.state.text}</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
